d25a93889e53a590326fae5f2a2fd2485bd7ee40
Avijit Dey <avijitdey2402@gmail.com>
2023-04-15 00:34:48 +0530

    update files in day 5 dir

---
+++ day5 Password Generator/appending_to_string.py
@@ -0,0 +1,5 @@
+s = ""
+for i in range(10):
+    s += str(i)
+print(s)
+print(type(s))
--- day5 Password Generator/main.py
+++ day5 Password Generator/main.py
@@ -20,17 +20,37 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 # 52 letters randint
 # 10 numbers randint
 # 9 symbols randint
-rand_list = []
+
+rand_string = ""
 
 for _ in range(nr_letters):
     # rand_order = random.randint(1,3) # 1 -> letters, 2 -> numbers, 3 -> symbol
-        rand_list.append(random.choice(numbers))
+    rand_string += random.choice(letters)
 for _ in range(nr_numbers):
-        rand_list.append
-print(rand_list)
+    rand_string += random.choice(numbers)
+for _ in range(nr_symbols):
+    rand_string += random.choice(symbols)
+print(rand_string)
 
+# EASY LEVEL COMPLETED TIL HERE
 # i will make letters+numbers+symbols format (easy then I will randomize it) : this way clone will be cleaner
+# NOW HARD LEVEL:
 
+# random.sample() method returns a list of non-duplicate elements from a sequence
+# and the len of this list is the 2nd positional argument (mandatory to write)
+rand_order_string_list = random.sample(rand_string, len(rand_string)) # sample() method makes a lIST
+password = ""
+for ele in rand_order_string_list:
+    password += ele
+print(password)
+# HARD LEVEL COMPLETED -- WORKS FINE
+
+
+# new_rand_str = ""
+# total_len = len(rand_string)
+# for _ in range(total_len):
+#     new_rand_str = random.choice(rand_string)
+    # won't work -- duplicate choices 
 
 
 
